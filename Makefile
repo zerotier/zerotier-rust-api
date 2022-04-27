36c26163213466082483ef0c0a11dbab18cd94ec
Erik Hollensbe <git@hollensbe.org>
2022-04-27 07:08:15 -0700

    openapiv3 clients generated from progenitor (erikh fork w/ fixes)
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 central:
-	@DESCRIPTION="OpenAPI bindings to the ZeroTier Central (https:\/\/my.zerotier.com) service" bash generate.sh central zerotier-central-api
+	bash generate.sh central zerotier-central-api
 	cd zerotier-central-api && cargo build && cargo clean
 
 service:
-	@DESCRIPTION="OpenAPI bindings to the ZeroTierOne JSON API" bash generate.sh service zerotier-one-api
+	bash generate.sh service zerotier-one-api
 	cd zerotier-one-api && cargo build && cargo clean
 
 .PHONY: central service
--- generate.sh
+++ generate.sh
@@ -2,21 +2,9 @@
 
 set -eo pipefail
 
-if [ "x$1" = "x" ]
+if [ "x$1" = "x" -o "x$2" = "x" ]
 then
   echo "Please read this script before executing it"
-  exit 1
-fi
-
-if [ "x$VERSION" = "x" ]
-then
-  echo "Please supply a version in VERSION= in plain semver format (e.g., 1.2.3)"
-  exit 1
-fi
-
-if [ "x$DESCRIPTION" = "x" ]
-then
-  echo "Please supply a package description in DESCRIPTION="
   exit 1
 fi
 
@@ -24,16 +12,4 @@ PACKAGE=$1
 PREFIX=$2
 
 HOST=${HOST:-docs.zerotier.com}
-
-rm -rf ./${PREFIX}
-mkdir -p ./${PREFIX}
-docker pull openapitools/openapi-generator-cli:latest
-docker run --rm -u $(id -u):$(id -g) -v ${PWD}/${PREFIX}:/swagger openapitools/openapi-generator-cli generate \
-  --package-name ${PREFIX} \
-  -i http://${HOST}/openapi/${PACKAGE}v1.json \
-  -g rust \
-  -o /swagger
-
-sed -e "s/@@PACKAGE@@/${PREFIX}/g" Cargo.toml.template | \
-  sed -e "s/@@VERSION@@/${VERSION}/g" | \
-  sed -e "s/@@DESCRIPTION@@/${DESCRIPTION}/g" > ${PREFIX}/Cargo.toml
+curl -sSL http://${HOST}/openapi/${PACKAGE}v1.json > ${PREFIX}/openapi.json
