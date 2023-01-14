5e78ef51f7e1f120abf1f978aca9ea5eab84d005
Sean OMeara <sean@sean.io>
2023-01-14 13:48:39 +0100

    using progenitor::Generator::default
    Signed-off-by: Sean OMeara <sean@sean.io>

--- zerotier-one-api/build.rs
+++ zerotier-one-api/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let src = PathBuf::from_str("openapi.json").unwrap();
     let file = std::fs::File::open(src).unwrap();
     let spec = serde_json::from_reader(file).unwrap();
-    let mut generator = progenitor::Generator::new();
+    let mut generator = progenitor::Generator::default();
 
     let content = generator.generate_text(&spec).unwrap();
 
